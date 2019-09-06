1a03ababc2ca335e16ce57ab951f51a254f2088a
Anna <any.suhor@gmail.com>
2019-09-06 16:58:58 +0300

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,26 +1,31 @@
 import random
-def is_valid_number():
-    if s.isdigit() and s > 1 and s < 100
-        return True
-def main():
-    num = random.random(1,100)
-    guess_num = False
-    guess = input('Guess a number from 1 to 100:')
-    num_guess = 0
-    while not guess_num:
-        if not is_valid_nu(guess):
-            guess < num:
-            guess = str(input("I won't count that one. A number between 1 and 100:"))
-        else:
-            num_guess += 1
-            guess = int(guess)
-        if guess < num:
-            guess = str(input("Too low. Guess again"))
-        elif guess > num:
-            guess = str(input("Too high. Guess again"))
-        else:
-            print("You got it in " + num_guess + "guesses!")
-            guess_num = True
-        print("Thanks for playing")
 
+def is_valid_number(s):
+    if s.isdigit() and 1 <= int(s) <=100:
+        return True
+    else:
+        return False
+
+def main():
+    number = random.randint(1,100)
+    guessed_number = False
+    guess = input("Guess a number from 1 to 100: ")
+    number_guess = 0
+    while not guessed_number:
+        if not is_valid_number(guess):
+            guess = str(input("I won't count that one. A number between 1 and 100: "))
+        else:
+            number_guess +=1
+            guess = int(guess)
+        if guess < number:
+            guess = str(input("Too low. Guess again: "))
+        elif guess > number:
+            guess = str(input("Too high. Guess again:  "))
+        else:
+            print("You got it in" ,number_guess," guesses!")
+            guessed_number = True
+    print("Thanks for playing.")
+
+if __name__ == '__main__':
+    main()
 
